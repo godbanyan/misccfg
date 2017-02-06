c942f45d2eb882e16d002aba0652f599a1d5f7a6
banyan <rong_z@163.com>
2017-02-06 10:04:36 +0800

    add bash_aliases

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ if [ -e ~/.vimrc ] || [ -L ~/.vimrc ]; then
     rm ~/.vimrc
 fi
 ln -s $curpath/vimrc ~/.vimrc
+
+if [ -e ~/.bash_aliases ] || [ -L ~/.bash_aliases ]; then
+    rm ~/.bash_aliases
+fi
+ln -s $curpath/bash_aliases ~/.bash_aliases
+
